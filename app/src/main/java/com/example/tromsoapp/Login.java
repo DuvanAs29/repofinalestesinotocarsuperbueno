46d4c0c77769d69d0e31d3664fd10ab7a18b9330
DtorresM21 <128758437+DtorresM21@users.noreply.github.com>
2023-07-18 20:37:33 -0500

    se crea el login

--- app/src/main/java/com/example/tromsoapp/Login.java
+++ app/src/main/java/com/example/tromsoapp/Login.java
@@ -2,12 +2,18 @@ package com.example.tromsoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     EditText cajaNombreUsuario;
     EditText cajaContrasena;
+
+    Button botonlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,21 @@ public class Login extends AppCompatActivity {
 
         cajaNombreUsuario=findViewById(R.id.nombreUsuario);
         cajaContrasena=findViewById(R.id.contraseñaUsuario);
-        String nombreUsuario=cajaNombreUsuario.getText().toString();
-        String contraseñaUsuario=cajaContrasena.getText().toString();
+        botonlogin=findViewById(R.id.botonlogin);
+
+        botonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombreUsuario=cajaNombreUsuario.getText().toString();
+                String contraseñaUsuario=cajaContrasena.getText().toString();
+
+                Intent intent= new Intent(Login.this,Home.class);
+                //pasando datos a la nueva actividad
+                intent.putExtra("nombre",nombreUsuario);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
